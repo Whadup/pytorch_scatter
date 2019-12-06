99cbd643a078055a535e691bbcc3d3b2a5b098a6
Lukas Pfahler <lukas.pfahler@udo.edu>
2019-12-06 10:06:45 +0100

    test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import platform
 from setuptools import setup, find_packages
 from sys import argv
-
+import setuptools
 
 TORCH_MAJOR = 1
 TORCH_MINOR = 3
@@ -12,14 +12,29 @@ if platform.system() != 'Windows':
 
 if (TORCH_MAJOR > 1) or (TORCH_MAJOR == 1 and TORCH_MINOR > 2):
     extra_compile_args += ['-DVERSION_GE_1_3']
+class MyExtension(setuptools.extension.Extension):
+    def __init__(self, *args, **kwargs):
+        super(MyExtension, self).__init__(*args, **kwargs)
+        self.ARGS = args
+        self.KWARGS = kwargs
+    def __getattribute__(self, x):
+        if x in ("ARGS", "KWARGS"):
+            return super(MyExtension, self).__getattribute__(x)
+        try:
+            from torch.utils.cpp_extension import CppExtension
+        except:
+            return ""
+        try:
+            return CppExtension(*self.ARGS, **self.KWARGS).__getattribute__(x)
+        except:
+            return ""
 
 def ext_modules_lazy():
-    try:
-        from torch.utils.cpp_extension import CppExtension
-        yield CppExtension('torch_scatter.scatter_cpu', ['cpu/scatter.cpp'],
-                 extra_compile_args=extra_compile_args)
-    except:
-        pass
+    # from torch.utils.cpp_extension import CppExtension
+    yield MyExtension('torch_scatter.scatter_cpu', ['cpu/scatter.cpp'],
+        extra_compile_args=extra_compile_args)
+    #'torch_scatter.scatter_cpu', ['cpu/scatter.cpp'],
+    #        
 
 def cmd_class_lazy():
     yield "dummy", ""
@@ -39,7 +54,7 @@ install_requires = ['torch']
 setup_requires = ['pytest-runner']
 tests_require = ['pytest', 'pytest-cov']
 
-print(setup(
+setup(
     name='torch_scatter',
     version=__version__,
     description='PyTorch Extension Library of Optimized Scatter Operations',
@@ -57,4 +72,4 @@ print(setup(
     ext_modules=list(ext_modules_lazy()),
     cmdclass={x:y for x, y in cmd_class_lazy()},
     packages=find_packages(),
-))
+)
