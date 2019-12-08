f79b47d5bb959ac67517d93daa18685e6856d5f0
Lukas Pfahler <lukas.pfahler@udo.edu>
2019-12-08 17:57:08 +0100

    try

--- setup.py
+++ setup.py
@@ -13,27 +13,8 @@ if (TORCH_MAJOR > 1) or (TORCH_MAJOR == 1 and TORCH_MINOR > 2):
 	extra_compile_args += ['-DVERSION_GE_1_3']
 
 
-
-class MyExtension(setuptools.extension.Extension):
-	def __init__(self, *args, **kwargs):
-		super(MyExtension, self).__init__(*args, **kwargs)
-		self.ARGS = args
-		self.KWARGS = kwargs
-	def __getattribute__(self, x):
-		print(x)
-		if x in ("ARGS", "KWARGS", "_convert_pyx_sources_to_lang"):
-			return super(MyExtension, self).__getattribute__(x)
-		try:
-			from torch.utils.cpp_extension import CppExtension
-		except:
-			return ""
-		try:
-			return CppExtension(*self.ARGS, **self.KWARGS).__getattribute__(x)
-		except:
-			return ""
-
 ext_modules = [
-	MyExtension('torch_scatter.scatter_cpu', ['cpu/scatter.cpp'],
+	setuptools.Extension('torch_scatter.scatter_cpu', ['cpu/scatter.cpp'],
 				 extra_compile_args=extra_compile_args)
 ]
 
@@ -54,6 +35,11 @@ def my_build_ext(pars):
 			import torch
 			a = torch.utils.cpp_extension.BuildExtension(*self.ARGS, **self.KWARGS)
 			self.__dict__.update(a.__dict__)
+			from torch.utils.cpp_extension import CppExtension
+			b = CppExtension('torch_scatter.scatter_cpu', ['cpu/scatter.cpp'],
+				extra_compile_args=extra_compile_args)
+			self.include_dirs = b.include_dirs
+			self.language = b.language
 	return build_ext(pars)
 
 cmdclass = {'build_ext': my_build_ext}
@@ -74,7 +60,7 @@ __version__ = '1.4.0'
 url = 'https://github.com/rusty1s/pytorch_scatter'
 
 install_requires = ['torch']
-setup_requires = ['torch', 'pytest-runner']
+setup_requires = ['torch','pytest-runner']
 tests_require = ['pytest', 'pytest-cov']
 
 setup(
